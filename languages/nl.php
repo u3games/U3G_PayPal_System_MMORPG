84d622c44b2deba1a2b12f5e4baa9f213b894199
koen <dasoldierr2@hotmail.com>
2016-02-01 20:54:26 +0100

    Update karma language

--- languages/nl.php
+++ languages/nl.php
@@ -11,6 +11,7 @@ $lang = array();
 
 $lang['character_name'] = 'karakter naam:';
 $lang['character_button'] = 'karakter bevestigen';
+$lang['select_option'] = 'Selecteer optie';
 
 $lang['message_1'] = 'Sorrie, karakter';
 $lang['message_2'] = 'bestaat niet.';
@@ -19,6 +20,9 @@ $lang['message_4'] = 'bestaat.';
 $lang['message_5'] = 'Haal munten:';
 $lang['message_6'] = 'munten';
 $lang['message_7'] = 'SANDBOX MODE';
+$lang['message_8'] = 'Verwijder';
+$lang['message_9'] = 'karma';
+$lang['message_10'] = 'Vewijder alle karma';
 
 $lang['recallform_1'] = 'karakter is online! Verbreek de verbinding om verder te gaan met de donatie.';
 $lang['recallform_2'] = 'Uw karakter naam moet minimaal 3 karakters bevatten.';
@@ -26,6 +30,7 @@ $lang['recallform_3'] = 'Je vergat een karakter naam te typen.';
 $lang['recallform_4'] = 'Je mag maximum 35 characters gebruiken.';
 $lang['recallform_5'] = 'Kan gamesever database niet selecteren.';
 $lang['recallform_6'] = 'Er kan geen verbinding gemaakt worden naar de server.';
+$lang['recallform_7'] = 'Selecteer een donatie optie.';
 
 $lang['done_1'] = 'Dank u voor het doneren, controleer nu de donatie in je karakter.';
 $lang['done_2'] = 'Als de donatie niet verschijnt op je karakter, Neem dan contact op met de administratie.';
