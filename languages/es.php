177606ad440bc6c50dceae7afc7aa30b6d3089b6
koen <dasoldierr2@hotmail.com>
2016-02-01 20:55:25 +0100

    Update karma language

--- languages/es.php
+++ languages/es.php
@@ -11,6 +11,7 @@ $lang = array();
 
 $lang['character_name'] = 'Nombre del Personaje:';
 $lang['character_button'] = 'Confirmar Personaje';
+$lang['select_option'] = 'Select option';
 
 $lang['message_1'] = 'Lo siento, el personaje';
 $lang['message_2'] = 'no existe.';
@@ -19,13 +20,17 @@ $lang['message_4'] = 'existe.';
 $lang['message_5'] = 'Seleccione Donación:';
 $lang['message_6'] = 'coins';
 $lang['message_7'] = 'SANDBOX MODE';
+$lang['message_8'] = 'Remove';
+$lang['message_9'] = 'karma';
+$lang['message_10'] = 'Remove all karma';
 
 $lang['recallform_1'] = 'El personaje esta conectado! Por favor, desconéctelo para poder realizar la donación.';
 $lang['recallform_2'] = 'Por favor, introduzque un nombre con más de 3 caracteres.';
 $lang['recallform_3'] = 'No has introducido el nombre del personaje, inténtelo de nuevo.';
-$lang['recallform_4'] = 'Has superado el máximo de 35 caracteres.';
+$lang['recallform_4'] = 'Maximum of 35 characters is allowed.';
 $lang['recallform_5'] = 'Ha habido un error al intentar conectar con el Game Server.';
 $lang['recallform_6'] = 'Ha habido un error al intentar conectar con la Base de Datos';
+$lang['recallform_7'] = 'Select a donation option.';
 
 $lang['done_1'] = 'Gracias por la donación realizada, ahora revisa que el item ha sido enviado al personaje indicado.';
 $lang['done_2'] = 'Si no habéis recibido el item, contacte con la administración antes de realizar una nueva donación.';
